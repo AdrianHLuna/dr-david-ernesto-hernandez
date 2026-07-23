50896a449e0765257005aa6e45c2acbdd21a9136
AdrianHLuna <locielf@gmail.com>
2026-07-23 11:22:11 -0600

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,6 @@
 import type { Metadata } from "next";
 import "./globals.css";
 import FloatingButtons from "@/components/FloatingButtons";
-import GoogleAnalytics from "@/components/GoogleAnalytics";
 import Header from "@/components/Header";
 import Footer from "@/components/Footer";
 import { doctor } from "@/data/doctor";
@@ -43,8 +42,54 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="es" className="scroll-smooth">
+      <head>
+        {/* Google Tag Manager */}
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-PQ5RTJF4');`,
+          }}
+        />
+        {/* Etiqueta Google Ads */}
+        <script async src="https://www.googletagmanager.com/gtag/js?id=AW-18345087001" />
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+
+              gtag('config', 'AW-18345087001');
+            `,
+          }}
+        />
+        {/* Etiqueta de Google Analytics */}
+        <script async src="https://www.googletagmanager.com/gtag/js?id=G-N1BM7ZXS47" />
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+
+              gtag('config', 'G-N1BM7ZXS47');
+            `,
+          }}
+        />
+      </head>
       <body className="antialiased flex flex-col min-h-screen">
-        <GoogleAnalytics />
+        {/* Google Tag Manager (noscript) */}
+        <noscript>
+          <iframe
+            src="https://www.googletagmanager.com/ns.html?id=GTM-PQ5RTJF4"
+            height="0"
+            width="0"
+            style={{ display: "none", visibility: "hidden" }}
+          />
+        </noscript>
         <Header />
         <main className="flex-grow">
           {children}
@@ -55,3 +100,4 @@ export default function RootLayout({
     </html>
   );
 }
+
